30de1b6d3789a504c7c1baae348fedb6356a55b2
Aashish Karki <aashish.y2z@gmail.com>
2018-12-05 14:05:58 +0545

    Add unquie indicator generator

--- js/Indicator.js
+++ js/Indicator.js
@@ -1,2 +1,8 @@
+// create unique indicator each time for unique element to be used
+
+function getUniqueIndicator() {
+  return new Date().getTime();
+}
+
 // Add indicators here and register them in Constant
 const IMAGE_INDICATOR = 'SOMETHING';
