cbc66029427cc0da60aa6d9828a92cb041056686
Aashish Karki <aashish.y2z@gmail.com>
2018-12-05 14:48:12 +0545

    Update indicator

--- js/Indicator.js
+++ js/Indicator.js
@@ -1,7 +1,7 @@
 // create unique indicator each time for unique element to be used
 
 function getUniqueIndicator() {
-  return new Date().getTime();
+  return new Date().getTime() + Math.floor(Math.random() * (1000 - 0) + 0);
 }
 
 // Add indicators here and register them in Constant
